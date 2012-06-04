707bc92ac5d765addec7cabbf1a12c9d70025ce8
dderaedt <dderaedt@adobe.com>
2012-06-04 11:28:02 +0200

    First commit
    A simple brackets extension to share create a gist from selection

---
+++ README.md
@@ -0,0 +1,9 @@
+brackets togist extension
+===
+
+A simple brackets extension to share easily create a Github Gist from the current selection. To install, place in ```brackets/src/extensions/user``` folder. 
+
+Usage
+=====
+Select some text, and then select Create Gist from the Edit menu.
+
---
+++ main.js
@@ -0,0 +1,74 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+
+/** Simple extension to create a gist from selection */
+define(function (require, exports, module) {
+    'use strict';
+
+
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        EditorManager  = brackets.getModule("editor/EditorManager"),
+        Menus          = brackets.getModule("command/Menus");
+
+
+    var SUCCESS_MSG = "Gist successfully created at:";
+    var ERROR_MSG = "Unable to create gist. Don't ask.";
+    var EMPTY_MSG = "Empty selection";
+    var GITHUB_API_URL = "https://api.github.com/gists";
+    var MY_COMMAND_ID = "togist.toGist";
+    var MENU_NAME = "Create Gist";
+
+    var defaultDescription = "Gist created from Brackets";
+
+    
+    function handleAction() {
+  
+        // retrieve selection
+        var selectedText = EditorManager.getFocusedEditor().getSelectedText();
+
+        if(selectedText==""){
+            window.alert(EMPTY_MSG);
+            return;
+        }
+
+        // Gist description to be sent to github
+        var postdata={
+              "description": defaultDescription,
+              "public": true,
+              "files": {
+                "mycode.js": {
+                  "content": selectedText
+                }
+              }
+            };
+
+
+        var postdataString=JSON.stringify(postdata);
+        
+
+        // send to github
+        $.ajax({
+            url: GITHUB_API_URL,
+            type:"POST",
+            dataType:"json",
+            data:postdataString,
+
+            error:function(data){
+                window.alert(ERROR_MSG);
+            },
+
+            success:function(data){
+                window.alert(SUCCESS_MSG+"\n"+data.html_url);
+                window.open(data.html_url);
+            }
+        });
+
+    }
+
+
+    CommandManager.register(MENU_NAME, MY_COMMAND_ID, handleAction);
+    
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuItem("menu-togist-toGist", MY_COMMAND_ID);
+    
+});
